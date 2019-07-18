9cc63605ea9ae6f9746f3b2435d46835d190dad8
Clark Chen <yyunqingchenn@gmail.com>
2019-07-18 01:16:05 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+type="text/javascript" ;
+src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js";
+
+$("#text").append("this is so freaking cool.");
+
+});
