b52b1f9740886b18beb8b8ec6a23a27c5a82eff2
Clark Chen <yyunqingchenn@gmail.com>
2019-07-18 01:34:04 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-
-type="text/javascript";
-
-src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js";
-
+  
 $("#text").append("this is so freaking cool.");
 
 });
